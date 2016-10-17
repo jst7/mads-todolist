88a75be866babe66b4ee6bdaa82911112b80f589
jst7 <jorge.segovia.tormo@gmail.com>
2016-10-17 21:55:21 +0200

    añadida base de los test

---
+++ test/services/UsuariosServiceTest.java
@@ -0,0 +1,67 @@
+package services;
+
+import play.db.Database;
+import play.db.Databases;
+import play.db.jpa.*;
+import org.junit.*;
+import org.dbunit.*;
+import org.dbunit.dataset.*;
+import org.dbunit.dataset.xml.*;
+import org.dbunit.operation.*;
+import java.io.FileInputStream;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import services.*;
+
+
+
+public class UsuariosServiceTest {
+
+    static Database db;
+    static JPAApi jpa;
+    JndiDatabaseTester databaseTester;
+
+    @BeforeClass
+    static public void initDatabase() {
+        db = Databases.inMemoryWith("jndiName", "DefaultDS");
+        // Necesario para inicializar el nombre JNDI de la BD
+        db.getConnection();
+        // Se activa la compatibilidad MySQL en la BD H2
+        db.withConnection(connection -> {
+            connection.createStatement().execute("SET MODE MySQL;");
+        });
+        jpa = JPA.createFor("memoryPersistenceUnit");
+    }
+
+    @Before
+    public void initData() throws Exception {
+        databaseTester = new JndiDatabaseTester("DefaultDS");
+        IDataSet initialDataSet = new FlatXmlDataSetBuilder().build(new
+        FileInputStream("test/resources/usuarios_dataset.xml"));
+        databaseTester.setDataSet(initialDataSet);
+        databaseTester.setTearDownOperation(DatabaseOperation.DELETE);
+        databaseTester.onSetup();
+    }
+
+    @After
+    public void clearData() throws Exception {
+        databaseTester.onTearDown();
+    }
+
+    @AfterClass
+    static public void shutdownDatabase() {
+        jpa.shutdown();
+        db.shutdown();
+    }
+
+    @Test
+    public void prueba() {
+    	jpa.withTransaction(() -> {
+    		boolean bolea=true;
+			assertTrue(bolea);
+        });
+    }
+}

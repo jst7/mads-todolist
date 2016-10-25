6d67ee350850c59096b55323d329a42c00cadea5
jst7 <jorge.segovia.tormo@gmail.com>
2016-10-25 12:32:10 +0200

    TIC21 añadirTarea añadida vista, controlador,routes

--- app/controllers/TareasController.java
+++ app/controllers/TareasController.java
@@ -24,4 +24,38 @@ public class TareasController extends Controller {
             return ok(listaTareas.render(tareas, usuario));
         }
     }
+
+    @Inject FormFactory formFactory;
+    @Transactional
+    public Result crearTareaFormulario(Integer usuarioId) {
+        Usuario usuario = UsuariosService.findUsuario(usuarioId);
+        if (usuario == null) {
+            return notFound("Usuario no encontrado");
+        } else {
+        return ok(crearTareaFormulario.render(formFactory.form(Tarea.class),"",usuario));
+    }
+   }
+
+    @Transactional
+    public Result crearTarea(Integer usuarioId) {
+
+        Usuario usuario = UsuariosService.findUsuario(usuarioId);
+
+        Form<Tarea> task = formFactory.form(Tarea.class).bindFromRequest();
+        if(task.hasErrors()){
+            return badRequest(crearTareaFormulario.render(formFactory.form(Tarea.class), "Errores en los campos de Tarea", usuario));
+        }
+        Tarea tarea = task.get();
+
+            try{
+            Tarea tareaMas = new Tarea(tarea.descripcion, usuario);
+            tareaMas = TareasService.crearTarea(tareaMas);
+            usuario.tareas.add(tareaMas);
+
+            return ok(crearTareaFormulario.render(formFactory.form(Tarea.class), "Tarea añadida", usuario));                   
+            }
+            catch(Exception e){
+            return badRequest(crearTareaFormulario.render(formFactory.form(Tarea.class), "Tarea no añadida", usuario));  
+            }
+   }
 }

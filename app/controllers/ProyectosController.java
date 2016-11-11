891c0b544d33b65810492be925ecebbe93f3de39
Adrián Gosálvez <ajgm3@alu.ua.es>
2016-11-11 12:58:34 +0100

    Crear proyecto (sin asignación de tarea)

--- app/controllers/ProyectosController.java
+++ app/controllers/ProyectosController.java
@@ -18,35 +18,26 @@ public class ProyectosController extends Controller {
         
     @Inject FormFactory formFactory;
     @Transactional
-    public Result crearProyectoFormulario(Integer idUsuario) {
-        Usuario usuario = UsuariosService.findUsuario(idUsuario);
-        if (usuario == null) {
-            return notFound("Usuario no encontrado");
-        } else {
-            return ok(crearProyectoFormulario.render(formFactory.form(Proyecto.class),"", usuario));
-        }
+    public Result crearProyectoFormulario() {
+        return ok(crearProyectoFormulario.render(formFactory.form(Proyecto.class),""));
     }
 
     @Transactional
-    public Result crearProyecto(Integer idUsuario) {
-        Usuario usuario = UsuariosService.findUsuario(idUsuario);
+    public Result crearProyecto() {
         Form<Proyecto> proyecto = formFactory.form(Proyecto.class).bindFromRequest();
         String msg = "";
 
         if(proyecto.hasErrors()){
-            return badRequest(crearProyectoFormulario.render(proyecto, "Los datos del formulario contienen errores", usuario));
+            return badRequest(crearProyectoFormulario.render(proyecto, "Los datos del formulario contienen errores"));
         } else {
-            Proyecto pAux           = proyecto.get();
-            Logger.debug("Proyecto nuevo: " + pAux.toString());
-            Proyecto proyectoNew    = new Proyecto(pAux.nombre, usuario);
-            //usuario.proyectos.add(proyectoNew);
-            ProyectosService.crearProyecto(proyectoNew);
             try {
+                Proyecto proyectoNew    = proyecto.get();
+                ProyectosService.crearProyecto(proyectoNew);
                 msg = "Proyecto creado correctamente";
-                return ok(crearProyectoFormulario.render(formFactory.form(Proyecto.class), msg, usuario));                   
+                return ok(crearProyectoFormulario.render(formFactory.form(Proyecto.class), msg));                   
             } catch (Exception e) {
                 msg = "Proyecto no creado";
-                return badRequest(crearProyectoFormulario.render(proyecto, msg, usuario));
+                return badRequest(crearProyectoFormulario.render(proyecto, msg));
             }
         }
     }

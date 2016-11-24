3ec1783499c9b1472fdff0afa5571badef11d6a1
Adrián Gosálvez <ajgm3@alu.ua.es>
2016-11-24 17:49:54 +0100

    Arreglo de estructura para la practica 4. Added js file

---
+++ public/javascripts/app.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function($) {
+
+	// selectors
+	/* Selectores de css para javsacript */
+
+	// init
+	/* Funcionalidades que se ejecutarán siempre al iniciar */
+
+
+	// triggers
+	/* Esto son los triggers, (eventos onALGO), onclick, onchange, mouseover etc) */
+
+
+	// functions
+	/* Aquí abajo van las funciones */
+
+
+})

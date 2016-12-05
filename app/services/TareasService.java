f64ec60074025cd372977b0cedef84acb6f2d1d5
Adrián Gosálvez <ajgm3@alu.ua.es>
2016-12-05 11:18:48 +0100

    Añado notificación desde tareas

--- app/services/TareasService.java
+++ app/services/TareasService.java
@@ -1,6 +1,10 @@
 package services;
 
+import play.*;
+import play.mvc.*;
+import play.db.jpa.*;
 import java.util.List;
+import java.util.Date;
 import java.util.ArrayList;
 
 import models.*;
@@ -17,6 +21,7 @@ public class TareasService {
     }
 
 	public static Tarea crearTarea(Tarea tarea) {
+        NotificacionService.crearNotificacion(new Notificacion(tarea.usuario.login, "Tarea", "Nueva tarea"));
     	return TareaDAO.create(tarea);
     }
     public static Tarea findTarea(Integer id) {
@@ -24,16 +29,18 @@ public class TareasService {
     }
 
     public static Tarea modificaTarea(Tarea tarea) {
+        NotificacionService.crearNotificacion(new Notificacion(tarea.usuario.login, "Tarea", "Modificación de tarea: " + tarea.id));
         TareaDAO.update(tarea);
         return tarea;
     }
     public static boolean deleteTarea(Integer id) {
-
-        try{
+        try {
+            Tarea tarea     = findTarea(id);
+            Usuario usuario = UsuariosService.findUsuario(tarea.usuario.id);
+            NotificacionService.crearNotificacion(new Notificacion(usuario.login, "Tarea", "Tarea " + id + " eliminada"));
             TareaDAO.delete(id);
             return true;
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             return false;
         }
     }
@@ -42,7 +49,7 @@ public class TareasService {
             tarea.proyecto = proyecto;
             TareaDAO.update(tarea);
             return true;
-        }catch(Exception e){
+        } catch (Exception e) {
             return false;
         }
     }
@@ -52,27 +59,22 @@ public class TareasService {
             tarea.proyecto = null;
             TareaDAO.update(tarea);
             return true;
-        }catch(Exception e){
+        } catch (Exception e) {
             return false;
         }
     }
 
     public static List<Tarea> busquedaTarea(String param) {
         List<Tarea> lista = TareaDAO.busquedaTarea(param);
-            
-
-            return lista;
+        return lista;
     }
     
     public static Integer CantidadTareasBusqueda(String param) {
         List<Tarea> lista = TareaDAO.busquedaTarea(param);
-            
-            if (lista==null){
-                return 0;
-            }
-
-            Integer cantidad = lista.size();
-            return cantidad;
+        if (lista == null) { 
+            return 0;
         }
-
+        Integer cantidad = lista.size();
+        return cantidad;
+    }
 }
